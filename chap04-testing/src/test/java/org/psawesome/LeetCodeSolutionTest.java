84caa7651a78fe8d6cffe6352c7d006cfd162621
ps <wiv821253@gmail.com>
2020-08-27 23:52:00 +0900

    add another version

--- chap04-testing/src/test/java/org/psawesome/LeetCodeSolutionTest.java
+++ chap04-testing/src/test/java/org/psawesome/LeetCodeSolutionTest.java
@@ -1,10 +1,16 @@
 package org.psawesome;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author ps [https://github.com/wiv33/reactive-programming-with-msa]
@@ -40,13 +46,13 @@ class LeetCodeSolutionTest {
 //            .peek(System.out::println)
             .limit(size)
             .count();
-    Assertions.assertEquals(6, count);
+    assertEquals(6, count);
   }
 
   @Test
   void testSubstringByDiscount() {
 //    if (strs.length == 0 || strs[0].isEmpty()) return "";
-     System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "12");
+    System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "12");
     final int size = strs[0].split("").length;
     final String actual = IntStream.iterate(size, s -> s - 1)
             .limit(size)
@@ -55,6 +61,20 @@ class LeetCodeSolutionTest {
             .findFirst()
             .orElse("");
 
-    Assertions.assertEquals("fl", actual);
+    assertEquals("fl", actual);
+  }
+
+  @Test
+  void testSecondFindLongestCommonPrefix() {
+    final var integer = new AtomicInteger(this.size);
+// System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "12");
+
+    final String actual = Stream.generate(() -> strs[0].substring(0, integer.getAndDecrement()))
+            .filter(s -> Arrays.stream(strs).skip(1).allMatch(f -> f.startsWith(s)))
+//            .findFirst()
+            .findAny()
+            .orElse("");
+
+    assertEquals("fl", actual);
   }
 }
